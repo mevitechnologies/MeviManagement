d820f112e10f44da1bf9728ec64ee930546fdd61
mevitechnologies <148612313+meviinternship@users.noreply.github.com>
2026-02-02 14:09:29 +0530

    Update settings.py

--- training_portal/settings.py
+++ training_portal/settings.py
@@ -150,7 +150,6 @@ LOGIN_URL = '/login/'
 
 CSRF_TRUSTED_ORIGINS = [
    
-    'https://mevimanagement-production-8528.up.railway.app'
+    'https://mevimanagement-production-8528.up.railway.app',
     "http://127.0.0.1:8000",
-    'https://mevimanagement-production-8528.up.railway.app'
 ]
